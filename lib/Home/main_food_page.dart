c3dd633afc2831a0ef39410890085ccb706a6b9c
Feranmi Emmanuel <feranmiemmanuel01@gmail.com>
2026-05-07 15:28:25 +0100

    Initial commit - food app

---
+++ lib/Home/main_food_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/Home/food_page_body.dart';
+import 'package:food_app/utils/colors.dart';
+import 'package:food_app/widgets/big_text.dart';
+import 'package:food_app/widgets/small_text.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MainFoodPageState createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return 
+    Scaffold(
+      body:  Column(
+        children: [
+           Container(
+            child:Container(
+              margin: EdgeInsets.only(top:45, bottom: 15),
+              padding: EdgeInsets.only(left: 20, right: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              BigText(text: "Bangladesh", color: AppColors.mainColor,),
+              Row(
+                children: [
+                  SmallText(text: "Narsingdi", color: Colors.black54,),
+                  Icon(Icons.arrow_drop_down_rounded)
+                ],
+              )
+
+            ],
+          ),
+          Center(
+            child: Container(
+              width: 45,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: AppColors.mainColor,
+              ),
+              child: Icon(Icons.search,color: Colors.white,),
+
+            ),
+          )
+        ],
+        
+      ),
+      
+     ), 
+     
+       ),
+        FoodPageBody(),
+        ],
+      
+      )
+      
+    );
+  }
+}
---
+++ lib/widgets/small_text.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class SmallText extends StatelessWidget {
+   Color? color;
+  final String text;
+  double size;
+  double height;
+   // ignore: use_super_parameters
+   SmallText({Key? key, this.color = const Color(0xFFccc7c5), 
+   required this.text,
+  this.size=12,
+  this.height=1.2
+  }):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      maxLines: 1,
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        color: color,
+       fontSize: size,
+       height: height,
+      ),
+    );
+  }
+}
